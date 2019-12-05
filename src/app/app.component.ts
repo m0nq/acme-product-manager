d8a87682caee676da29ec038908dcb5ea4222b82
Monk Wellington <forgetmenaught@gmail.com>
2019-12-05 15:41:12 -0800

    Update boilerplate files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cq-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apm';
+  title = 'Angular: Getting Started';
 }
