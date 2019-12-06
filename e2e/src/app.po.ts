67f28c77748ad7512f23ca89a42773159bc2f7f0
Monk Wellington <forgetmenaught@gmail.com>
2019-12-06 12:48:20 -0800

    Updated application prefix

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('pm-root h1')).getText() as Promise<string>;
+    return element(by.css('cq-root h1')).getText() as Promise<string>;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
+  selector: 'cq-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
