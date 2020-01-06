e2be8f3451a8bb9dca894d80d092c2168f95034f
Monk Wellington <forgetmenaught@gmail.com>
2020-01-05 22:21:14 -0800

    Refactored product feature module with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './product-list/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    ProductModule,
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
