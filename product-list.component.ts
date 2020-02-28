9f60e619f3cf6d4c0467c59469b23bde553f33be
Monk <m0nq@users.noreply.github.com>
2020-02-28 15:16:10 -0800

    Combine products and categories to display them together

---
+++ product-list.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories;
+
+  products$: Observable<Product[]> = this.productService.productsWithCategory$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+  constructor(private productService: ProductService) { }
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
