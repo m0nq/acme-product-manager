1a2fa2b01151e60aa39d160ac06da01802427c73
m0nq <forgetmenaught@gmail.com>
2020-03-05 22:25:39 -0800

    Using higher order observables

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { of, throwError } from 'rxjs';
+import { concatMap, map, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { Supplier } from './supplier';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,31 @@ import { throwError } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8)
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  suppliersWithConcatMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log(`concatMap source observable -> ${id}`)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  supplierWithMergeMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log(`mergeMap source Observable -> ${id}`)),
+      mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  supplierWithSwitchMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log(`switchMap source Observable -> ${id}`)),
+      switchMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  constructor(private http: HttpClient) {
+  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
