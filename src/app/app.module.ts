bef59e91cf7fe707f57a89e349d46c52d1253770
m0nq <forgetmenaught@gmail.com>
2020-03-17 12:26:19 -0700

    Add and initialize store in root and product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { StoreModule } from '@ngrx/store';
 // Imports for loading & configuring the in-memory web api
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ShellComponent } from './home/shell.component';
 import { MenuComponent } from './home/menu.component';
-import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './home/page-not-found.component';
-
+import { ShellComponent } from './home/shell.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductData } from './products/product-data';
 /* Feature Modules */
 import { UserModule } from './user/user.module';
 
@@ -22,6 +21,7 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
+    StoreModule.forRoot({}),
     UserModule,
     AppRoutingModule
   ],
@@ -34,4 +34,4 @@ import { UserModule } from './user/user.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
